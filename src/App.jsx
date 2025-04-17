598a2e99d0c8d86a114d9b9ef0e12a9ebde39c30
Thinh Lai <thinhlai1308@gmail.com>
2025-04-16 23:47:06 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
-import './App.css'
+import { puppyList } from './DATA.JS';
+import { useState } from 'react';
+import './App.css';
+
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null); 
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+  console.log(featuredPup)
+
   return (
-    <div>
-      
+    <div className="App">
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+
+      {
+        puppies.map((puppy) => {
+          return (
+            <p onClick={() => { setFeatPupId(puppy.id)}} key={puppy.id}>
+              {puppy.name}
+            </p>
+          );
+        })
+      }
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
